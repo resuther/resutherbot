bcf348c279c405706a4cb1247d8e1fefb98ab0c3
resuther <72550915+resuther@users.noreply.github.com>
2020-11-22 18:05:04 +1300

    Create avatar.py

---
+++ avatar.py
@@ -0,0 +1,6 @@
+@client.command()
+async def avatar(ctx, *, member: discord.Member=None):
+    if not member:
+        member = ctx.message.author
+    userAvatar = member.avatar_url
+    await ctx.send(userAvatar)
