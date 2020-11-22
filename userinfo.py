e92ab68bf66f8713d32b0b16abe8860971ffea0a
resuther <72550915+resuther@users.noreply.github.com>
2020-11-22 18:07:30 +1300

    Create userinfo.py

---
+++ userinfo.py
@@ -0,0 +1,23 @@
+@client.command()
+async def userinfo(ctx, member: discord.Member = None):
+    member = ctx.author if not member else member
+    roles = [role for role in member.roles]
+
+    embed = discord.Embed(color=member.color, timestamp=ctx.message.created_at)
+
+    embed.set_author(name=f"User Info - {member}")
+    embed.set_thumbnail(url=member.avatar_url)
+    embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
+
+    embed.add_field(name="ID:", value=member.id)
+    embed.add_field(name="Guild name:", value=member.display_name)
+
+    embed.add_field(name="Created at:", value=member.created_at.strftime("%a, %#d %B %Y, %I:%M %p UTC"))
+    embed.add_field(name="Joined at:", value=member.joined_at.strftime("%a, %#d %B %Y, %I:%M %p UTC"))
+
+    embed.add_field(name=f"Roles ({len(roles)})", value=" ".join([role.mention for role in roles]))
+    embed.add_field(name="Top role:", value=member.top_role.mention)
+
+    embed.add_field(name="Bot?", value=member.bot)
+
+    await ctx.send(embed=embed)
